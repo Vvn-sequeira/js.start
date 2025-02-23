3b9418edb43ffdb0d642c4416531c7d9679b25cb
Vivian Marcel Sequeira <svivianmarcel@gmail.com>
2025-02-23 21:14:33 +0530

    Add files via upload

---
+++ jsDatatype.js
@@ -0,0 +1,20 @@
+ "use strict" ; // treat all js code as newer version  
+
+
+ // alert("uello") // can't run .. bcoz we are in nodejs not in browser 
+
+ // number  => 2 to power 53 
+ // begint 
+ //string  
+ // boolean 
+ //NUll =? standAlone value  let value = null ;
+ // undefined  let value;
+ //symbol => unique 
+
+ //boject 
+
+ console.log(typeof " vivivan");  // string 
+ console.log(typeof null); // object 
+ console.log(typeof undefined); // undefined 
+
+ 
