9bbcc07a79e56569685f9a07a1968c800b4199ad
Vivian Marcel Sequeira <svivianmarcel@gmail.com>
2025-03-22 18:10:15 +0530

    .

---
+++ javaScript-part-2.js
@@ -0,0 +1,36 @@
+// let a = 23;
+// let b = 34;
+
+// console.log(`my total salary is ${a+b} rupeess for a month`);
+
+
+// let str = "ama";
+
+// if(str.charAt(0) == 'a' && str.length == 3)
+// {
+//       console.log("good string ");
+// }
+// else{
+//      console.log("bad string ");
+// }
+
+
+// console.error("this is an error msg");
+// console.warn("this is an error msg");
+// // alert("this is an error msg");
+// let a = prompt("enter u r name");
+// let b = prompt("enter u r age");
+
+
+// console.log(`the user name is ${a} and his/her age is ${b} .`);
+
+let a = 2323;
+let b = 3433;
+
+if(a%10 == 3 && b%10 == 3)
+{
+        console.log(`the no having ${a%10} has their last no `);
+}
+else{
+      console.log("it don't have same last digit no ");
+}
